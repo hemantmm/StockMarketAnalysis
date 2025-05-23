50387d028dae3dcd2f8d50cb3ebbde6a2d06d3bf
Hemant M Mehta <hemant29mehta@gmail.com>
2025-05-23 10:04:29 +0530

    fix: url for backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app= express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://stock-market-analysis-five-lake.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 }))
